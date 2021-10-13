ac643a0276df70f4065ac3372ab7245e3b7abd7e
Oleksiy Ryndin <alex.ryndin@gmail.com>
2021-10-13 15:08:47 -0400

    21w42a

--- src/main/java/ryndinol/mymod/mixin/ExampleMixin.java
+++ src/main/java/ryndinol/mymod/mixin/ExampleMixin.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import ryndinol.mymod.MyMod;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
